0c23f270dac94022ef6268f040efa7a19dd5fd16
Чан Тоан <c.toan@corp.mail.ru>
2020-05-01 14:01:46 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int cost_ticket=100_000;
+        int one_bonus=20;
+        int personal_bonus=cost_ticket/one_bonus;
+        System.out.println(personal_bonus);
+    }
+}
