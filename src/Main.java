0bb1b3a16e5a08b0e02e034ddaaa77c7573b1699
Чан Тоан <c.toan@corp.mail.ru>
2020-05-01 14:49:35 +0300

    Исправил названия переменных

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
 
-        int cost_ticket=100_000;
-        int one_bonus=20;
-        int personal_bonus=cost_ticket/one_bonus;
-        System.out.println(personal_bonus);
+        int costTicket = 100_000;
+        int oneBonus = 20;
+        int personalBonus = costTicket / oneBonus;
+        System.out.println(personalBonus);
     }
 }
